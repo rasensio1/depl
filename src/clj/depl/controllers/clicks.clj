4cd8d9b025e45a5b3b75704b1d8413201ba9c99e
Ryan Asensio <ryan@pkcsecurity.com>
2018-06-15 13:39:03 -0700

    Creates a click on POST.

--- src/clj/depl/controllers/clicks.clj
+++ src/clj/depl/controllers/clicks.clj
@@ -1,5 +1,5 @@
 (ns depl.controllers.clicks
   (:require [depl.models.click :as m-clk]))
 
-(defn create [username]
+(defn create [{{:keys [username]} :params}]
   (m-clk/create! username))
--- src/clj/depl/handler.clj
+++ src/clj/depl/handler.clj
@@ -3,13 +3,17 @@
             [depl.controllers.clicks :as c-clk]
             [compojure.route :refer [resources]]
             [ring.util.response :refer [resource-response]]
-            [ring.middleware.reload :refer [wrap-reload]]))
+            [ring.middleware.reload :refer [wrap-reload]]
+            [ring.middleware.params :as params]
+            [ring.middleware.keyword-params :as kw-params]))
 
 (defroutes routes
   (GET "/" [] (resource-response "index.html" {:root "public"}))
-  (POST "/clicks" [username] (c-clk/create username))
+  (POST "/clicks" [_] c-clk/create)
   (resources "/"))
 
 (def dev-handler (-> #'routes wrap-reload))
 
-(def handler routes)
+(def handler (-> routes
+                 kw-params/wrap-keyword-params
+                 params/wrap-params))
