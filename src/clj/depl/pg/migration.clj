001e8d3f43f15ed480f8f1dccb8c2927a64b4a6d
Ryan Asensio <ryan@pkcsecurity.com>
2018-06-15 12:59:54 -0700

    Fix sql stuff.

--- src/clj/depl/pg/migration.clj
+++ src/clj/depl/pg/migration.clj
@@ -1,22 +1,20 @@
 (ns depl.pg.migration
-  (:require [clojure.java.jdbc :as sql]))
-
-(def db-url
-  (or (System/getenv "DATABASE_URL")
-      "postgresql://localhost:5432/depl"))
+  (:require [depl.pg.utils :as utils]
+            [clojure.java.jdbc :as sql]))
 
 (defn migrated? []
-  (-> sql/query [(str "select count(*) from information_schema.tables "
-                      "were table_name='clicks'")]
-      :first :count pos?))
+  (-> (sql/query utils/db-url [(str "select count(*) from information_schema.tables "
+                                    "where table_name='clicks'")])
+      first :count pos?))
 
 (defn migrate []
   (when (not (migrated?))
     (print "Creating database structure...") (flush)
-    (sql/db-do-commands db-url
+    (sql/db-do-commands utils/db-url
                         (sql/create-table-ddl
                          :clicks
                          [[:id :serial "PRIMARY KEY"]
+                          [:username :varchar "NOT NULL"]
                           [:created_at :timestamp "NOT NULL" "DEFAULT CURRENT_TIMESTAMP"]])))
   (println " done"))
 
